b285651de9fcacadc0e6dde786e4aabfc064d98c
pigeonOwO <jonas.stiller@t-online.de>
2024-04-03 17:13:00 +0200

    implemented argparse for server

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 import http.server
 import socketserver
-import sys
+import argparse
 
-PORT = int(sys.argv[1])
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--port", default=8080, type=int, dest="port")
+args = parser.parse_args()
+
+PORT = args.port
 Handler = http.server.SimpleHTTPRequestHandler
 httpd = socketserver.TCPServer(("", PORT), Handler)
 print("serving at port", PORT)
