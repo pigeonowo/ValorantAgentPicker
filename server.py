eae37a56242b8e8ef45ddef0b71db6da49f94302
pigeonOwO <jonas.stiller@t-online.de>
2024-04-03 16:57:12 +0200

    first commit

---
+++ server.py
@@ -0,0 +1,9 @@
+import http.server
+import socketserver
+import sys
+
+PORT = int(sys.argv[1])
+Handler = http.server.SimpleHTTPRequestHandler
+httpd = socketserver.TCPServer(("", PORT), Handler)
+print("serving at port", PORT)
+httpd.serve_forever()
